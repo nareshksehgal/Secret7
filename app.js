97048d545ef19825738dab134caef7b5b188e0ac
nareshksehgal <nareshksehgal@gmail.com>
2020-05-21 19:15:09 -0700

    added google authentication

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ mongoose.set("useCreateIndex", true);
 //for encryption
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleID: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -51,17 +53,27 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapi.com/oauth2/v3/userinfo"
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
+
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      console.log("successfully found " + profile.id);
       return cb(err, user);
     });
   }
@@ -77,8 +89,9 @@ app.get('/auth/google',
 app.get('/auth/google/secrets',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
+    console.log("Successfully authenticated");
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/secrets');
   });
 
 app.get("/login", function(req, res) {
